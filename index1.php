30c0b8deacf23561ff1dca4de115f9521cf2fdfc
Alejandro <alejandroluisacosta95@gmail.com>
2024-08-26 11:36:07 +0200

    add first exercise

---
+++ index1.php
@@ -0,0 +1,13 @@
+<?php
+    $rooms = [
+        ["ID" => 1, "Name" => "Single Bed", "Number" => "11", "Price" => "120", "Discount" => "10"],
+        ["ID" => 2, "Name" => "Single Bed", "Number" => "12", "Price" => "120", "Discount" => "10"],
+        ["ID" => 3, "Name" => "Double Bed", "Number" => "13", "Price" => "145", "Discount" => "5"],
+        ["ID" => 4, "Name" => "Double Superior", "Number" => "21", "Price" => "165", "Discount" => "0"],
+        ["ID" => 5, "Name" => "Suite", "Number" => "31", "Price" => "200", "Discount" => "10"],
+    ];
+?>
+
+<pre>
+    <?php print_r($rooms); ?>
+</pre>
