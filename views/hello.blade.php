5beee93f5be229fc7d7f92548f1aec6ace3ee9fd
Alejandro <alejandroluisacosta95@gmail.com>
2024-08-28 12:49:07 +0200

    add exercise 10

--- views/hello.blade.php
+++ views/hello.blade.php
@@ -1,1 +1,11 @@
-<h1>Hello {{ $name }}</h1>
+<h1>Rooms</h1>
+@foreach ($rooms as $room)
+    <ol>
+        <li>{{ $room['name']}}</li>
+        <ul>
+            <li>Number: {{ $room['id'] }}</li>
+            <li>Price: ${{ $room['rate'] }}</li>
+            <li>Discount: {{ $room['discount'] }}%</li>
+        </ul>
+    </ol>
+@endforeach
